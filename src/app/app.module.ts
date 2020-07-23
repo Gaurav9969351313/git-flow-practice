191427990ea7c4880e5aba907ecdba92c2203443
gaurav9969351313 <gaurav.cdac16@gmail.com>
2020-07-23 11:51:55 +0530

    google analytics completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GoogleAnalyticsComponent } from './google-analytics/google-analytics.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoogleAnalyticsComponent
   ],
   imports: [
     BrowserModule,
